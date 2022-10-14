520321480902506ddd4fa9db8109f4edcf405012
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-14 10:10:18 +0100

    Tests with jest implemented. First test passed.

---
+++ _tests_/pet.test.js
@@ -0,0 +1,7 @@
+const Pet = require('../src/pet');
+
+describe('constructor', () => {
+    it('returns an object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
+    })
+});
---
+++ constructor.js
@@ -0,0 +1,3 @@
+function Pet (name) {
+    this.name = 'Fido'
+}
