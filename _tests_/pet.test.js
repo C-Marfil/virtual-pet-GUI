375cfe87c70ab0fd1be3ec5351c672740b323a41
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-11-02 10:15:48 +0000

    Refactored tests and code

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,122 +1,140 @@
-const Pet = require('./src/pet');
+const Pet = require('../src/pet');
+
 
 describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
-    })
+    });
     it('checks initial age of pet', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toBe(0);
-    })
-});
-describe('test the growUp function', () => {
-    it('increments age of the pet by numberOfYears', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.age).toEqual(1);
-    })
-    it('checks the fitness increments', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.fitness).toEqual(7);
-        expect(pet.hunger).toEqual(5);
-        pet.growUp();
-        expect(pet.fitness).toEqual(4);
-        expect(pet.hunger).toEqual(10);
-    })
-});
-describe('The use of the walk function', () => {
-    it('increases fitness and should not be over 10', () => {
-        const pet = new Pet('Fido')
-        pet.growUp();
-        expect(pet.fitness).toEqual(7);
-        pet.walk();
-        expect(pet.fitness).toEqual(10);
-    });
-    it('Throws an error if dead', () =>{
-        const pet1 = new Pet('lola');
-        pet1.age = 30;
-        expect(() => pet1.PTSession()).toThrow('Your pet is no longer alive BIATCH');
     });
 });
-describe('The use of the feedASnack & feedAPizza functions', () => {
-    it('Checks that a snack lowers hunger by 3 and is never below 0', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        expect(pet.hunger).toEqual(5);
-        pet.feedAPizza();
-        pet.feedASnack();
-        expect(pet.hunger).toEqual(0);
+    describe('Declare Pets', () => {
+        let pet;
+        let parent;
+        let child;
+        beforeEach(() => {
+            pet = new Pet('Fido');
+            parent = new Pet('Pal');
+            child = new Pet('Pololo');
+        });
+    describe('test the growUp function', () => {
+        it('increments age of the pet by 1', () => {
+            pet.growUp();
+            
+            expect(pet.age).toEqual(1);
+        });
+        it('checks how growUp modifies fitness and hunger', () => {
+            pet.growUp();
+            
+            expect(pet.fitness).toEqual(7);
+            expect(pet.hunger).toEqual(5);
+           
+            pet.growUp();
+            
+            expect(pet.fitness).toEqual(4);
+            expect(pet.hunger).toEqual(10);
+        });
     });
-    it('Checks the feedAPizza function', () => {
-        const pet = new Pet('difo');
-        pet.growUp();
-        pet.feedAPizza();
-        expect(pet.hunger).toBe(0);
+    describe('The use of the walk function', () => {
+        it('increases fitness and should not be over 10', () => {
+            pet.fitness = 10;
+            pet.walk();
+            expect(pet.fitness).toEqual(10);
+        });
+        it('Throws an error if dead', () =>{
+            pet.age = 30;
+            expect(() => pet.PTSession()).toThrow('Your pet is no longer alive!');
+        });
     });
-    it('Throws an error if dead', () =>{
-        const pet = new Pet('lola');
-        pet.age = 30;
-        expect(() => pet.feedASnack()).toThrow('Your pet is no longer alive BIATCH');
-    });
-});
-describe('The use of the checkup function', () => {
-    it('It tells you when it is hungry, low fitness, both or ok', () => {
-        const pet = new Pet('Fido');
-        pet.growUp();
-        pet.fitness = 5;
-        expect(pet.checkUp()).toBe('I am hungry AND I need a walk')
-        const pet1 = new Pet('Fido');
-        pet1.age = 15;
-        pet1.hunger = 0;
-        pet1.fitness = 3;
-        expect(pet1.checkUp()).toBe('I need a walk')
-        const pet2 = new Pet('Fido');
-        pet2.growUp();
-        expect(pet2.checkUp()).toBe('I am hungry')
-        const pet3 = new Pet('Fido');
-        pet3.feedASnack();
-        expect(pet3.checkUp()).toBe('I feel great!')
-    })
-    it('expects a dead err message if biatch is ded', () => {
-        const biatch = new Pet('Biatch');
-        biatch.age = 30;
-        expect(() => biatch.checkUp()).toThrow('Your pet is no longer alive BIATCH')
-    })
-});
-describe('The getter isAlive', () => {
-    it('returns if pet is alive', () => {
-        const pet = new Pet('fido');
-        expect(pet.isAlive).toBe(true);
-        const pet1 = new Pet('Poo');
-        pet1.age = 30;
-        expect(pet1.isAlive).toBe(false);
-        const pet2 = new Pet('Sorra')
-        pet2.age = 20;
-        pet2.fitness = 0;
-        expect(pet2.isAlive).toBe(false);
-        const pet3 = new Pet('Luju');
-        pet3.age = 29;
-        pet3.PTSession();
-        pet3.hunger = 10;
-        expect(pet3.isAlive).toBe(false);
-    })
-});
-describe('The act of having babies', () => {
-    it('ADOPTION: Creates a new pet and adopts it as a child', () => {
-        const parent = new Pet('Dave');
-        const child = new Pet('Pololo');
-        
-        parent.adoptChild('Pololo');
+    describe('The use of the feedASnack & feedAPizza functions', () => {
+        it('Checks that a snack lowers hunger by 3', () => {
+            pet.hunger = 5;
+            
+            pet.feedASnack();
+            
+            expect(pet.hunger).toEqual(2);
+        });
+        it('feedAPizza method, which reduces hunger completely and can never be below 0', () => {
+            pet.hunger = 5;
+            
+            pet.feedAPizza();
+            
+            expect(pet.hunger).toBe(0);
+            
+            pet.feedAPizza();
 
-        expect(parent.children).toBe('Pololo');
-    })
-    it('PREGNANCY: Actually has a baby', () =>{
-        const parent = new Pet('Parent');
+            expect(pet.hunger).toBe(0);
+        });
+        it('Throws an error if dead', () =>{
+            pet.age = 30;
 
-        parent.haveBaby('Polola');
+            expect(() => pet.feedASnack()).toThrow('Your pet is no longer alive!');
+        });
+    });
+    describe('The use of the checkup function', () => {
+        it('It tells you when it is hungry, low fitness, both or ok', () => {
+            pet.fitness = 5;
+            pet.hunger = 3;
+            
+            expect(pet.checkUp()).toBe('I am hungry AND I need a walk');
+            
+            pet.age = 15;
+            pet.hunger = 0;
+            pet.fitness = 3;
+            
+            expect(pet.checkUp()).toBe('I need a walk');
+            
+            pet.age = 0;
+            pet.hunger = 5;
+            pet.fitness = 10;
 
-        expect(parent.children[0].name).toBe('Polola');
-        expect(parent.children[0]).toBeInstanceOf(Object);
-    })
-})
+            expect(pet.checkUp()).toBe('I am hungry');
+            
+            pet.hunger = 0;
+            pet.fitness = 10;
+
+            expect(pet.checkUp()).toBe('I feel great!');
+        });
+        it('expects a dead err message if pet is dead', () => {
+            pet.age = 30;
+
+            expect(() => pet.checkUp()).toThrow('Your pet is no longer alive!');
+        });
+    });
+    describe('The getter isAlive', () => {
+        it('returns if pet is alive', () => {
+            expect(pet.isAlive).toBe(true);
+    
+            pet.age = 30;
+            
+            expect(pet.isAlive).toBe(false);
+            
+            pet.age = 20;
+            pet.fitness = 0;
+
+            expect(pet.isAlive).toBe(false);
+
+            pet.age = 29;
+            pet.fitness = 10;
+            pet.hunger = 10;
+
+            expect(pet.isAlive).toBe(false);
+        });
+    });
+    describe('The act of having babies', () => {
+        it('ADOPTION: Creates a new pet and adopts it as a child', () => {
+            parent.adoptChild('Pololo');
+
+            expect(parent.children).toBe('Pololo');
+        });
+        it('PREGNANCY: Actually generates a baby', () =>{
+
+            parent.haveBaby('Polola');
+
+            expect(parent.children[0].name).toBe('Polola');
+            expect(parent.children[0]).toBeInstanceOf(Object);
+        });
+    });
+});
--- src/pet.js
+++ src/pet.js
@@ -19,8 +19,8 @@ const LIFESPAN = 30;
 Pet.prototype = {
     get isAlive() {
       return this.age < LIFESPAN && this.hunger < MAXIMUM_HUNGER && this.fitness > MINIMUM_FITNESS;
-    }
-  }
+    },
+  };
 
 // PROTOTYPE METHODS FOR PET OBJECTS //
 //GROWING UP: It adds years and hunger. It reduces fitness.//
@@ -31,15 +31,16 @@ const FASTING = 5;
 
 Pet.prototype.growUp = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
+        throw new Error('Your pet is no longer alive!');
+    };
+
     this.age += A_YEAR;
     this.fitness -= SOFA_TIME;
     this.hunger += FASTING;
     if(this.fitness <= MINIMUM_FITNESS) {
         this.fitness = MINIMUM_FITNESS;
-    }
-}
+    };
+};
 
 //WALK: It increases fitness up to 10 //
 
@@ -48,40 +49,46 @@ const A_WORKOUT = 4;
 
 Pet.prototype.walk = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
+        throw new Error('Your pet is no longer alive!');
+    };
+
     this.fitness += A_WORKOUT;
     if(this.fitness > MAXIMUM_FITNESS) {
         this.fitness = MAXIMUM_FITNESS;
-    }
+    };
 }
 Pet.prototype.PTSession = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
-    this.fitness = MAXIMUM_FITNESS;
-}
+        throw new Error('Your pet is no longer alive!');
+    };
 
-//FEEDASNACK: Reduces hunger by 3//
+    this.fitness = MAXIMUM_FITNESS;
+};
+
+/* FEEDASNACK: Reduces hunger by 3
+FEEDAPIZZA: Reduces hunger to 0 */
 
 const MINIMUM_HUNGER = 0;
 const A_SNACK = 3;
 
 Pet.prototype.feedASnack = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
+        throw new Error('Your pet is no longer alive!');
+    };
+
     this.hunger -= A_SNACK;
     if(this.hunger < MINIMUM_HUNGER) {
         this.hunger = MINIMUM_HUNGER;
-    }
-}
+    };
+};
+
 Pet.prototype.feedAPizza = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
+        throw new Error('Your pet is no longer alive!');
+    };
+
     this.hunger = MINIMUM_HUNGER;
-}
+};
 
 //CHECKUP: Tells you if your pet needs food, a walk or if it's ok//
 
@@ -90,8 +97,9 @@ const FITNESS_DANGER = 5;
 
 Pet.prototype.checkUp = function(){
     if (!this.isAlive) {
-        throw new Error('Your pet is no longer alive BIATCH!');
-    }
+        throw new Error('Your pet is no longer alive!');
+    };
+
     if(this.fitness <= FITNESS_DANGER && this.hunger >= HUNGER_DANGER) {
         return 'I am hungry AND I need a walk';
     } else if(this.fitness <= FITNESS_DANGER) {
@@ -100,27 +108,19 @@ Pet.prototype.checkUp = function(){
         return 'I am hungry';
     } else { 
         return 'I feel great!';
-    }
-}
+    };
+};
 
 //ADOPT A CHILD: The parent pet adopts another existing pet. //
 
 Pet.prototype.adoptChild = function(childToBe) {
     this.children += childToBe;
-}
+};
 
 //PREGNANCY: Actually have a baby that is the same instance as parent. //
 
 Pet.prototype.haveBaby = function(name) {
      this.children.push(new Pet(name));
-}
-
-//TIME: Playing with intervals. Don't know how to test it. //
-
-setInterval(function(){
-    if(isAlive){
-        growUp();
-    }
-}, 60000);
+};
 
 module.exports =  Pet;
