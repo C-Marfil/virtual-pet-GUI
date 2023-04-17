09b25e2e8f2058f69ba8a1ecfa51f532f2430fa8
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-04-17 08:55:15 +0100

    added styles

--- src/controller.js
+++ src/controller.js
@@ -1,181 +1,183 @@
 (function exportController() {
-    let nightMode = false;
+  let nightMode = false;
 
-    function Controller (pet) {
-        this.pet = pet;
+  function Controller(pet) {
+    this.pet = pet;
 
-        document.querySelector('#button-snack').addEventListener('click', () => {
-            let getPetImg = document.getElementById('pet');
+    document.querySelector("#button-snack").addEventListener("click", () => {
+      let getPetImg = document.getElementById("pet");
 
-            this.pet.feedASnack();
-            this.lowerHunger();
+      this.pet.feedASnack();
+      this.lowerHunger();
 
-            if (this.pet.evolved === true) {
-            getPetImg.src = 'images/EvoFood.png';
-            };
-            if (this.pet.evolved === false) {
-            getPetImg.src = 'images/SnackPet.png';
-            };
-        });
+      if (this.pet.evolved === true) {
+        getPetImg.src = "images/EvoFood.png";
+      }
+      if (this.pet.evolved === false) {
+        getPetImg.src = "images/SnackPet.png";
+      }
+    });
 
-        document.querySelector('#button-pt').addEventListener('click', () => {
-            let getPetImg = document.getElementById('pet');
-            
-            this.pet.PTSession();
-            this.lowerFitness();
+    document.querySelector("#button-pt").addEventListener("click", () => {
+      let getPetImg = document.getElementById("pet");
 
-            if (this.pet.evolved === false) {
-            getPetImg.src = 'images/PTPet.png';
-            };
-            if (this.pet.evolved === true) {
-            getPetImg.src = 'images/EvoPT.png';
-            };
-        });
+      this.pet.PTSession();
+      this.lowerFitness();
 
-        document.querySelector('#button-pizza').addEventListener('click', () => {
-            let getPetImg = document.getElementById('pet');
-            
-            this.pet.feedAPizza();
-            this.lowerHunger();
+      if (this.pet.evolved === false) {
+        getPetImg.src = "images/PTPet.png";
+      }
+      if (this.pet.evolved === true) {
+        getPetImg.src = "images/EvoPT.png";
+      }
+    });
 
-            if (this.pet.evolved === true) {
-            getPetImg.src = 'images/EvoFood.png';
-            };
-            if (this.pet.evolved === false) {
-            getPetImg.src = 'images/PizzaPet.png';
-            };
-        });
+    document.querySelector("#button-pizza").addEventListener("click", () => {
+      let getPetImg = document.getElementById("pet");
 
-        document.querySelector('#button-exercise').addEventListener('click', () => {
-            let getPetImg = document.getElementById('pet');
-            
-            this.pet.walk();
-            this.lowerFitness();
-            
-            if (this.pet.evolved === false) {
-            getPetImg.src = 'images/SportPet.png';
-            };
-            if (this.pet.evolved === true) {
-            getPetImg.src = 'images/EvoPT.png';
-            };
-        });
+      this.pet.feedAPizza();
+      this.lowerHunger();
 
-        document.querySelector('#button-evo').addEventListener('click', () => {
-            let getPetImg = document.getElementById('pet');
+      if (this.pet.evolved === true) {
+        getPetImg.src = "images/EvoFood.png";
+      }
+      if (this.pet.evolved === false) {
+        getPetImg.src = "images/PizzaPet.png";
+      }
+    });
 
-            if(this.pet.age < 15) {
-                this.renderMessage(`Your pet is too young!`)
-            };
-            if (this.pet.age >= 15) {
-                getPetImg.src = 'images/Evolution.png';
-                this.pet.isEvolved;
-            };
-        });
+    document.querySelector("#button-exercise").addEventListener("click", () => {
+      let getPetImg = document.getElementById("pet");
 
-        document.querySelector('#button-nightmode').addEventListener('click', (event) => {
-            const getComputer = document.querySelector('#computer');
-            const getButton = document.querySelector('#button-nightmode');
+      this.pet.walk();
+      this.lowerFitness();
 
-            if(!nightMode) {
-                getComputer.src = `images/nightTimeComputer.png`;
-                getButton.innerHTML = 'ZZZ';
-                
-                nightMode = true;
-            } else { 
-                getComputer.src = `images/dayTimeComputer.png`;
-                getButton.innerHTML = 'DAY';
-                nightMode = false;
-            };
-        });
-    };
+      if (this.pet.evolved === false) {
+        getPetImg.src = "images/SportPet.png";
+      }
+      if (this.pet.evolved === true) {
+        getPetImg.src = "images/EvoPT.png";
+      }
+    });
 
-    Controller.prototype = {
-        lowerFitness() {
-            const getFitness = document.getElementById('fitness');
-            let getFitnessStyle = getFitness.style;
-            
-            if (this.pet.fitness === 10) {
-                getFitnessStyle.backgroundImage = `url('../images/10fitness.png')`;
-            };
-            if (this.pet.fitness === 7) {
-                getFitnessStyle.backgroundImage = `url('../images/7fitness.png')`;
-            };
-            if (this.pet.fitness === 4) {
-                getFitnessStyle.backgroundImage = `url('../images/4fitness.png')`;
-            };
-            if (this.pet.fitness === 1) {
-                getFitnessStyle.backgroundImage = `url('../images/1fitness.png')`;
-            };
-            if (this.pet.isAlive === false) {
-                getFitnessStyle.backgroundImage = `url('../images/death.png')`;
-                getFitnessStyle.backgroundSize = '100%';
-            };
-        },
+    document.querySelector("#button-evo").addEventListener("click", () => {
+      let getPetImg = document.getElementById("pet");
 
-        lowerHunger() {
-            const getHunger = document.getElementById('hunger');
-            let getHungerStyle = getHunger.style;
-            
-            if (this.pet.hunger === 0) {
-                    getHungerStyle.backgroundImage = `url('../images/0hunger.png')`;
-            };
-            if (this.pet.hunger === 3) {
-                    getHungerStyle.backgroundImage = `url('../images/3hunger.png')`;
-            };
-            if (this.pet.hunger === 6) {
-                    getHungerStyle.backgroundImage = `url('../images/6hunger.png')`;
-            };
-            if (this.pet.hunger === 9) {
-                    getHungerStyle.backgroundImage = `url('../images/9hunger.png')`;
-            };
-            if (this.pet.isAlive === false) {
-                    getHungerStyle.backgroundImage = `url('../images/death.png')`;
-                    getHungerStyle.backgroundSize = '100%';
-            };
-        },
+      if (this.pet.age < 15) {
+        this.renderMessage(`Your pet is too young!`);
+      }
+      if (this.pet.age >= 15) {
+        getPetImg.src = "images/Evolution.png";
+        this.pet.isEvolved;
+      }
+    });
 
-        renderMessage(message) {
-            const newMessageElement = document.createElement('div');
-            const divMessage = document.querySelector('#divMessage');
-            
-            newMessageElement.id = 'message';
-            newMessageElement.innerHTML = message;
-            divMessage.appendChild(newMessageElement);
-            
-            setTimeout(() => {
-                divMessage.removeChild(newMessageElement);
-            }, 3000);
-        },
+    document
+      .querySelector("#button-nightmode")
+      .addEventListener("click", (event) => {
+        const getComputer = document.querySelector("#computer");
+        const getButton = document.querySelector("#button-nightmode");
 
-        initialisePet() {
-            const pet = this.pet;
-            let getPetImg = document.getElementById('pet');
-            const intervalGrow = window.setInterval(() => {
-                if (pet.age === 1) {
-                this.renderMessage(`Your pet is now ${pet.age} year old!`);
-            } else if (pet.age === 0) {
-                this.renderMessage(`Your pet is ALIVE!`);
-            } 
-            else {
-                this.renderMessage(`Your pet is now ${pet.age} years old!`);
-            }
-                pet.growUp();
-                this.lowerFitness();
-                this.lowerHunger();
+        if (!nightMode) {
+          getComputer.src = `images/nightTimeComputer.png`;
+          getButton.innerHTML = "ZZZ";
 
-                if(pet.isAlive === false){
-                    window.clearInterval(intervalGrow);
-                    getPetImg.src = 'images/death.png';
-                    this.renderMessage(`Your pet JUST DROPPED DEAD!`);
-                    getPetImg.style.width = '40%';
-                };
-            }, 5000);
-        },
-    };
-
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Controller;
+          nightMode = true;
         } else {
-        window.Controller = Controller;
-    };
-}());
+          getComputer.src = `images/dayTimeComputer.png`;
+          getButton.innerHTML = "DAY";
+          nightMode = false;
+        }
+      });
+  }
+
+  Controller.prototype = {
+    lowerFitness() {
+      const getFitness = document.getElementById("fitness");
+      let getFitnessStyle = getFitness.style;
+
+      if (this.pet.fitness === 10) {
+        getFitnessStyle.backgroundImage = `url('../images/10fitness.png')`;
+      }
+      if (this.pet.fitness === 7) {
+        getFitnessStyle.backgroundImage = `url('../images/7fitness.png')`;
+      }
+      if (this.pet.fitness === 4) {
+        getFitnessStyle.backgroundImage = `url('../images/4fitness.png')`;
+      }
+      if (this.pet.fitness === 1) {
+        getFitnessStyle.backgroundImage = `url('../images/1fitness.png')`;
+      }
+      if (this.pet.isAlive === false) {
+        getFitnessStyle.backgroundImage = `url('../images/death.png')`;
+        getFitnessStyle.backgroundSize = "100%";
+      }
+    },
+
+    lowerHunger() {
+      const getHunger = document.getElementById("hunger");
+      let getHungerStyle = getHunger.style;
+
+      if (this.pet.hunger === 0) {
+        getHungerStyle.backgroundImage = `url('../images/0hunger.png')`;
+      }
+      if (this.pet.hunger === 3) {
+        getHungerStyle.backgroundImage = `url('../images/3hunger.png')`;
+      }
+      if (this.pet.hunger === 6) {
+        getHungerStyle.backgroundImage = `url('../images/6hunger.png')`;
+      }
+      if (this.pet.hunger === 9) {
+        getHungerStyle.backgroundImage = `url('../images/9hunger.png')`;
+      }
+      if (this.pet.isAlive === false) {
+        getHungerStyle.backgroundImage = `url('../images/death.png')`;
+        getHungerStyle.backgroundSize = "100%";
+      }
+    },
+
+    renderMessage(message) {
+      const newMessageElement = document.createElement("div");
+      const divMessage = document.querySelector("#divMessage");
+
+      newMessageElement.id = "message";
+      newMessageElement.innerHTML = message;
+      divMessage.appendChild(newMessageElement);
+
+      setTimeout(() => {
+        divMessage.removeChild(newMessageElement);
+      }, 3000);
+    },
+
+    initialisePet() {
+      const pet = this.pet;
+      let getPetImg = document.getElementById("pet");
+      const intervalGrow = window.setInterval(() => {
+        if (pet.age === 1) {
+          this.renderMessage(`Your pet is now ${pet.age} year old!`);
+        } else if (pet.age === 0) {
+          this.renderMessage(`Your pet is ALIVE!`);
+        } else {
+          this.renderMessage(`Your pet is now ${pet.age} years old!`);
+        }
+
+        pet.growUp();
+        this.lowerFitness();
+        this.lowerHunger();
+
+        if (pet.isAlive === false) {
+          window.clearInterval(intervalGrow);
+          getPetImg.src = "images/death.png";
+          this.renderMessage(`Your pet JUST DROPPED DEAD!`);
+          getPetImg.style.width = "40%";
+        }
+      }, 5000);
+    },
+  };
+
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Controller;
+  } else {
+    window.Controller = Controller;
+  }
+})();
--- src/pet.js
+++ src/pet.js
@@ -1,104 +1,120 @@
 (function exportPet() {
-    const STARTING_AGE = 0;
-    const LIFESPAN = 30;
-    const MAXIMUM_FITNESS = 10;
-    const MAXIMUM_HUNGER = 10;
-    const MINIMUM_HUNGER = 0;
-    const MINIMUM_FITNESS = 0;
-    const HUNGER_DANGER = 3;
-    const FITNESS_DANGER = 5;
-    const FITNESS_REDUCED_BY_GROWTH = 3;
-    const HUNGER_INCREASED_BY_GROWTH = 3;
-    const SMALL_FITNESS_INCREASE = 4;
+  const STARTING_AGE = 0;
+  const LIFESPAN = 30;
+  const MAXIMUM_FITNESS = 10;
+  const MAXIMUM_HUNGER = 10;
+  const MINIMUM_HUNGER = 0;
+  const MINIMUM_FITNESS = 0;
+  const HUNGER_DANGER = 3;
+  const FITNESS_DANGER = 5;
+  const FITNESS_REDUCED_BY_GROWTH = 3;
+  const HUNGER_INCREASED_BY_GROWTH = 3;
+  const SMALL_FITNESS_INCREASE = 4;
 
-    function Pet(name) {
-        this.name = name;
-        this.age = STARTING_AGE;
-        this.hunger = MINIMUM_HUNGER;
-        this.fitness = MAXIMUM_FITNESS;
-        this.children = [];
-        this.evolved = false;
-    };
+  function Pet(name) {
+    this.name = name;
+    this.age = STARTING_AGE;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
+    this.children = [];
+    this.evolved = false;
+  }
 
-    Pet.prototype = {
-        get isAlive() {
-        return this.age < LIFESPAN && this.hunger < MAXIMUM_HUNGER && this.fitness > MINIMUM_FITNESS;
-        },
+  Pet.prototype = {
+    get isAlive() {
+      return (
+        this.age < LIFESPAN &&
+        this.hunger < MAXIMUM_HUNGER &&
+        this.fitness > MINIMUM_FITNESS
+      );
+    },
 
-        get isEvolved() {
-            return this.evolved = true;
-        },
-    };
+    get isEvolved() {
+      return (this.evolved = true);
+    },
+  };
 
-    Pet.prototype.growUp = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
-
-        this.age += 1;
-        this.hunger = Math.max(this.hunger += HUNGER_INCREASED_BY_GROWTH, MINIMUM_HUNGER);
-        this.fitness = Math.max(this.fitness -= FITNESS_REDUCED_BY_GROWTH, MINIMUM_FITNESS);
-    };
-
-    Pet.prototype.walk = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
-
-        this.fitness = Math.min(this.fitness += SMALL_FITNESS_INCREASE, MAXIMUM_FITNESS);
+  Pet.prototype.growUp = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
     }
 
-    Pet.prototype.PTSession = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
+    this.age += 1;
+    this.hunger = Math.max(
+      (this.hunger += HUNGER_INCREASED_BY_GROWTH),
+      MINIMUM_HUNGER
+    );
+    this.fitness = Math.max(
+      (this.fitness -= FITNESS_REDUCED_BY_GROWTH),
+      MINIMUM_FITNESS
+    );
+  };
 
-        this.fitness = MAXIMUM_FITNESS;
-    };
+  Pet.prototype.walk = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
+    }
 
-    Pet.prototype.feedASnack = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
+    this.fitness = Math.min(
+      (this.fitness += SMALL_FITNESS_INCREASE),
+      MAXIMUM_FITNESS
+    );
+  };
 
-        this.hunger = Math.min(this.hunger -= HUNGER_INCREASED_BY_GROWTH, MAXIMUM_HUNGER);
-    };
+  Pet.prototype.PTSession = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
+    }
 
-    Pet.prototype.feedAPizza = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
+    this.fitness = MAXIMUM_FITNESS;
+  };
 
-        this.hunger = MINIMUM_HUNGER;
-    };
+  Pet.prototype.feedASnack = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
+    }
 
-    Pet.prototype.checkUp = function(){
-        if (!this.isAlive) {
-            throw new Error('Your pet is no longer alive!');
-        };
+    this.hunger = Math.min(
+      (this.hunger -= HUNGER_INCREASED_BY_GROWTH),
+      MAXIMUM_HUNGER
+    );
+  };
 
-        if (this.fitness <= FITNESS_DANGER && this.hunger >= HUNGER_DANGER) {
-            return 'I am hungry AND I need a walk';
-        } else if (this.fitness <= FITNESS_DANGER) {
-            return 'I need a walk';
-        } else if (this.hunger >= HUNGER_DANGER) {
-            return 'I am hungry';
-        } else { 
-            return 'I feel great!';
-        };
-    };
+  Pet.prototype.feedAPizza = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
+    }
 
-    Pet.prototype.adoptChild = function(childToBe) {
-        this.children += childToBe;
-    };
+    this.hunger = MINIMUM_HUNGER;
+  };
 
-    Pet.prototype.haveBaby = function(name) {
-        this.children.push(new Pet(name));
-    };
+  Pet.prototype.checkUp = function () {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive!");
+    }
 
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Pet;
-        } else {
-        window.Pet = Pet;
-        };
-}());
+    if (this.fitness <= FITNESS_DANGER && this.hunger >= HUNGER_DANGER) {
+      return "I am hungry AND I need a walk";
+    } else if (this.fitness <= FITNESS_DANGER) {
+      return "I need a walk";
+    } else if (this.hunger >= HUNGER_DANGER) {
+      return "I am hungry";
+    } else {
+      return "I feel great!";
+    }
+  };
+
+  Pet.prototype.adoptChild = function (childToBe) {
+    this.children += childToBe;
+  };
+
+  Pet.prototype.haveBaby = function (name) {
+    this.children.push(new Pet(name));
+  };
+
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Pet;
+  } else {
+    window.Pet = Pet;
+  }
+})();
