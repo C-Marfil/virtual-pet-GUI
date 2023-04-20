059c7862e6e34919611d7248f7e23b6de4961f35
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-04-20 19:59:38 +0100

    refactored controller constructor to class

--- src/controller.js
+++ src/controller.js
@@ -1,97 +1,96 @@
 (function exportController() {
   let nightMode = false;
 
-  function Controller(pet) {
-    this.pet = pet;
+  class Controller {
+    constructor(pet) {
+      this.pet = pet;
 
-    document.querySelector("#button-snack").addEventListener("click", () => {
-      let getPetImg = document.getElementById("pet");
+      document.querySelector("#button-snack").addEventListener("click", () => {
+        let getPetImg = document.getElementById("pet");
 
-      this.pet.feedASnack();
-      this.lowerHunger();
+        this.pet.feedASnack();
+        this.lowerHunger();
 
-      if (this.pet.evolved === true) {
-        getPetImg.src = "images/EvoFood.png";
-      }
-      if (this.pet.evolved === false) {
-        getPetImg.src = "images/SnackPet.png";
-      }
-    });
-
-    document.querySelector("#button-pt").addEventListener("click", () => {
-      let getPetImg = document.getElementById("pet");
-
-      this.pet.PTSession();
-      this.lowerFitness();
-
-      if (this.pet.evolved === false) {
-        getPetImg.src = "images/PTPet.png";
-      }
-      if (this.pet.evolved === true) {
-        getPetImg.src = "images/EvoPT.png";
-      }
-    });
-
-    document.querySelector("#button-pizza").addEventListener("click", () => {
-      let getPetImg = document.getElementById("pet");
-
-      this.pet.feedAPizza();
-      this.lowerHunger();
-
-      if (this.pet.evolved === true) {
-        getPetImg.src = "images/EvoFood.png";
-      }
-      if (this.pet.evolved === false) {
-        getPetImg.src = "images/PizzaPet.png";
-      }
-    });
-
-    document.querySelector("#button-exercise").addEventListener("click", () => {
-      let getPetImg = document.getElementById("pet");
-
-      this.pet.walk();
-      this.lowerFitness();
-
-      if (this.pet.evolved === false) {
-        getPetImg.src = "images/SportPet.png";
-      }
-      if (this.pet.evolved === true) {
-        getPetImg.src = "images/EvoPT.png";
-      }
-    });
-
-    document.querySelector("#button-evo").addEventListener("click", () => {
-      let getPetImg = document.getElementById("pet");
-
-      if (this.pet.age < 15) {
-        this.renderMessage(`Your pet is too young!`);
-      }
-      if (this.pet.age >= 15) {
-        getPetImg.src = "images/Evolution.png";
-        this.pet.isEvolved;
-      }
-    });
-
-    document
-      .querySelector("#button-nightmode")
-      .addEventListener("click", (event) => {
-        const getComputer = document.querySelector("#computer");
-        const getButton = document.querySelector("#button-nightmode");
-
-        if (!nightMode) {
-          getComputer.src = `images/nightTimeComputer.png`;
-          getButton.innerHTML = "ZZZ";
-
-          nightMode = true;
-        } else {
-          getComputer.src = `images/dayTimeComputer.png`;
-          getButton.innerHTML = "DAY";
-          nightMode = false;
+        if (this.pet.evolved === true) {
+          getPetImg.src = "images/EvoFood.png";
+        }
+        if (this.pet.evolved === false) {
+          getPetImg.src = "images/SnackPet.png";
         }
       });
-  }
 
-  Controller.prototype = {
+      document.querySelector("#button-pt").addEventListener("click", () => {
+        let getPetImg = document.getElementById("pet");
+
+        this.pet.PTSession();
+        this.lowerFitness();
+
+        if (this.pet.evolved === false) {
+          getPetImg.src = "images/PTPet.png";
+        }
+        if (this.pet.evolved === true) {
+          getPetImg.src = "images/EvoPT.png";
+        }
+      });
+
+      document.querySelector("#button-pizza").addEventListener("click", () => {
+        let getPetImg = document.getElementById("pet");
+
+        this.pet.feedAPizza();
+        this.lowerHunger();
+
+        if (this.pet.evolved === true) {
+          getPetImg.src = "images/EvoFood.png";
+        }
+        if (this.pet.evolved === false) {
+          getPetImg.src = "images/PizzaPet.png";
+        }
+      });
+
+      document.querySelector("#button-exercise").addEventListener("click", () => {
+        let getPetImg = document.getElementById("pet");
+
+        this.pet.walk();
+        this.lowerFitness();
+
+        if (this.pet.evolved === false) {
+          getPetImg.src = "images/SportPet.png";
+        }
+        if (this.pet.evolved === true) {
+          getPetImg.src = "images/EvoPT.png";
+        }
+      });
+
+      document.querySelector("#button-evo").addEventListener("click", () => {
+        let getPetImg = document.getElementById("pet");
+
+        if (this.pet.age < 15) {
+          this.renderMessage(`Your pet is too young!`);
+        }
+        if (this.pet.age >= 15) {
+          getPetImg.src = "images/Evolution.png";
+          this.pet.isEvolved;
+        }
+      });
+
+      document
+        .querySelector("#button-nightmode")
+        .addEventListener("click", (event) => {
+          const getComputer = document.querySelector("#computer");
+          const getButton = document.querySelector("#button-nightmode");
+
+          if (!nightMode) {
+            getComputer.src = `images/nightTimeComputer.png`;
+            getButton.innerHTML = "ZZZ";
+
+            nightMode = true;
+          } else {
+            getComputer.src = `images/dayTimeComputer.png`;
+            getButton.innerHTML = "DAY";
+            nightMode = false;
+          }
+        });
+    }
     lowerFitness() {
       const getFitness = document.getElementById("fitness");
       let getFitnessStyle = getFitness.style;
@@ -112,7 +111,7 @@
         getFitnessStyle.backgroundImage = `url('../images/death.png')`;
         getFitnessStyle.backgroundSize = "100%";
       }
-    },
+    }
 
     lowerHunger() {
       const getHunger = document.getElementById("hunger");
@@ -134,7 +133,7 @@
         getHungerStyle.backgroundImage = `url('../images/death.png')`;
         getHungerStyle.backgroundSize = "100%";
       }
-    },
+    }
 
     renderMessage(message) {
       const newMessageElement = document.createElement("div");
@@ -147,7 +146,7 @@
       setTimeout(() => {
         divMessage.removeChild(newMessageElement);
       }, 3000);
-    },
+    }
 
     initialisePet() {
       const pet = this.pet;
@@ -172,8 +171,9 @@
           getPetImg.style.width = "40%";
         }
       }, 5000);
-    },
-  };
+    }
+  }
+
 
   if (typeof module !== "undefined" && module.exports) {
     module.exports = Controller;
